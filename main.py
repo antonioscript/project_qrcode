5de35dd49ddeb072c85df198687da4baccc7fe61
Antônio Rocha <antoniojunior159@gmail.com>
2022-05-16 18:38:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 #Programa Simples: Só tirar as '#'
 #imagem = qrcode.make("link desejado")
-#imagem.save("qrcode-imagem.png")#Nome do arquivo que vai ser gerado
+#imagem.save("qrcode-imagem.png") #Nome do arquivo que vai ser gerado
 
-import qrcode
+import qrcode #Importe essa biblioteca
 
 print("===========GERADOR QRCODE ==========")
-link = input("Digite o Link que você quer gerar o QRCode: ")
+link = input("Digite o Link que você quer gerar o QRCode: ") #Coloque seu link aqui
 
 imagem = qrcode.make(link)
-imagem.save("nome_arquivo.png")
+imagem.save("nome_arquivo.png") #nome do arquivo que será criado
 
 print("QRCode Gerado!")
